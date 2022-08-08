0272374434a796f8bd5f33b11e1327eb0e06726c
AleksandraKepel <sannnia.pusha.97@mail.ru>
2022-08-09 01:05:21 +0300

    added files

---
+++ index.js
@@ -0,0 +1,37 @@
+// alert("Я JavaScript!");
+
+// let admin;
+// let name; 
+// name = "Джон";
+// admin = name;
+// alert( admin );
+
+// let ourPlanetName = "Земля";
+// let currentUserName = "Джон";
+
+let myName = "Ilya"; 
+alert( `hello ${1}` ); //hello 1
+alert( `hello ${"myname"}` ); //hello myname
+alert( `hello ${myName}` ); //hello Ilya
+
+// let ourName = prompt("Ваше имя?", "");
+// alert(ourName);
+
+// "" + 1 + 0 //10
+// "" - 1 + 0 //-1
+// true + false //1
+// 6 / "3" //2
+// "2" * "3" //6
+// 4 + 5 + "px" //9px
+// "$" + 4 + 5 //45
+// "4" - 2 //2
+// "4px" - 2 //NaN
+// "  -9  " + 5 //-9 5
+// "  -9  " - 5 // -14
+// null + 1 // 1
+// undefined + 1 // NaN
+// " \t \n" - 2 // -2
+
+// let a = +prompt("Первое число?", 1);
+// let b = +prompt("Второе число?", 2);
+// alert(a + b); // 3
